00ca6b4a93319ae76873dd8c0bfc814e6b248afa
Adam Weil <adamjweil@gmail.com>
2018-03-12 19:31:48 -0400

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -23,13 +23,4 @@ ActiveRecord::Schema.define(version: 20180203210609) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "username"
-    t.string "password_digest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
-  end
-
 end
