d0be923f311a3356e8f7c330202c8180d0dc1529
Adam Weil <adamjweil@gmail.com>
2018-03-12 19:58:35 -0400

    config

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,18 @@
 require_relative 'boot'
 
-require 'rails/all'
-
+# require 'rails/all'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
+require 'faker'
+require 'bcrypt'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -14,5 +25,13 @@ module Adamsapp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+  config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+    # Don't generate system test files.
+    config.generators.system_tests = nil
   end
 end
